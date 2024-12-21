67d295007f76634b7aad14a47fb61e54a33670a6
ygorgama <ygor.bio.2002@gmail.com>
2024-12-21 20:53:17 -0300

    config: Innitial configuration

---
+++ src/test/java/edu/projectjava/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.projectjava.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
