d29b10775e27bef56d1201ec0ff9d4c69ebadbb3
ygorgama <ygor.bio.2002@gmail.com>
2024-12-22 20:02:45 -0300

    feature: creation of OrderItem entity

---
+++ src/main/java/edu/projectjava/api/entities/OrderItem.java
@@ -0,0 +1,78 @@
+package edu.projectjava.api.entities;
+
+import java.util.Objects;
+
+import edu.projectjava.api.entities.pk.OrderItemPk;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+	@EmbeddedId
+	private OrderItemPk id;
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {}
+
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		this.id.setOrder(order);
+		this.id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	
+	public Order getOrder() {
+		return this.id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		this.id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return this.id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		this.id.setProduct(product);
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
--- src/main/java/edu/projectjava/api/entities/Product.java
+++ src/main/java/edu/projectjava/api/entities/Product.java
@@ -8,8 +8,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -19,16 +21,18 @@ public class Product implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	public Product() {}
+
+	public Product() {
+	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
@@ -81,6 +85,5 @@ public class Product implements Serializable {
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
-		
+
 }
